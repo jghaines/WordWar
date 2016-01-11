6cce6fb3d4eb26e36887f39aeda2f318d126f168
Jason Haines <github@jhaines.name>
2016-01-11 12:26:33 +1100

    Refactored scrabble letters to separate .js file

--- js/script.js
+++ js/script.js
@@ -1,33 +1,5 @@
-var scrabbleLetters = 
-	"A".repeat(9) +
-	"B".repeat(2) +
-	"C".repeat(2) +
-	"D".repeat(4) +
-	"E".repeat(12) +
-	"F".repeat(2) +
-	"G".repeat(3) +
-	"H".repeat(2) +
-	"I".repeat(9) +
-	"J".repeat(1) +
-	"K".repeat(1) +
-	"L".repeat(4) +
-	"M".repeat(2) +
-	"N".repeat(6) +
-	"O".repeat(8) +
-	"P".repeat(2) +
-	"Q".repeat(1) +
-	"R".repeat(6) +
-	"S".repeat(4) +
-	"T".repeat(6) +
-	"U".repeat(4) +
-	"V".repeat(2) +
-	"W".repeat(2) +
-	"X".repeat(1) +
-	"Y".repeat(2) +
-	"Z".repeat(1);
-
 function getRandomLetter() {
-    return scrabbleLetters.charAt(Math.floor(Math.random() * scrabbleLetters.length));
+    return getScrabbleLetter();
 }
 
 // whether the position selected for the letter is valid
