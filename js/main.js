43ddd30243c31ccbfa9751213e20ffd8490d084a
Jason Haines <github@jhaines.name>
2016-01-12 17:45:49 +1100

    Switchable app url

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ window.onload = function(){
 	log.setLevel('DEBUG');
 	log.info('main()');
 
-	var socket = io('http://localhost:3000');
+	//var app_url = 'https://wordwar.herokuapp.com/';
+	var app_url = 'http://localhost:3000';
+	var socket = io(app_url);
 	remote = new RemoteController(socket);
 	gameController = new GameController(remote);
 }
