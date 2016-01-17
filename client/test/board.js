78b9bae4e45b8d431c516e7ad2e7b24968abdfe8
Jason Haines <github@jhaines.name>
2016-01-18 09:14:10 +1100

    Remove BoardModel#loadBoard test - not working

--- client/test/board.js
+++ client/test/board.js
@@ -32,37 +32,3 @@ describe('BoardModel', function() {
 	});
 
 });
-
-describe('BoardModel', function() {	
-	var xhr, requests;
-
-	before(function () {
-	    xhr = sinon.useFakeXMLHttpRequest();
-	    requests = [];
-	    xhr.onCreate = function (req) { requests.push(req); };
-	});
-
-	after(function () {
-	    // Like before we must clean up when tampering with globals.
-	    xhr.restore();
-	});
-
-	describe('loadBoard()', function () {
-		it('should call load and do callback', function (done) {
-			this._boardModel = new BoardModel();
-			this._boardModel.onBoardLoaded( done );
-			this._boardModel.loadBoard( 'dummyUrl');
-
-			expect( requests.length ).to.equal( 1 ); // doesn't work
-			expect( requests[0].url ).match( 'dummyUrl' ); //
-		});
-	});
-/*
-	after(function () {
-		// When the test either fails or passes, restore the original
-		// jQuery ajax function (Sinon.JS also provides tools to help
-		// test frameworks automate clean-up like this)
-		jQuery.ajax.restore();
-	});
-*/
-});
