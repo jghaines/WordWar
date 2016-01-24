2507d2687deb2c3f6f0941cdab92be2fdc086c8d
Jason Haines <github@jhaines.name>
2016-01-24 19:03:36 +1100

    board2html.js script for simple text format to html board format Sample 2.txt board

---
+++ client/boards/board2html.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict';
+
+var readline = require('readline');
+
+console.log( '<tbody>' );
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line){
+	process.stdout.write( '\t<tr> ' );
+
+	line.split('').forEach( function( letter ) {
+		switch ( true ) {
+			case ( '.' == letter ) : process.stdout.write( "<td/>" ); break; // blank cell
+
+			case ( '<' == letter ) : process.stdout.write( "<td class='player-local'/>" ); break; 
+			case ( '>' == letter ) : process.stdout.write( "<td class='player-remote'/>" ); break; 
+
+			case ( '_' == letter ) : process.stdout.write( "<td class='block'/>" ); break; 
+			case ( '?' == letter ) : process.stdout.write( "<td class='wildcard' ww_value='?'/>" ); break;
+			case ( '2' == letter ) : process.stdout.write( "<td class='bonus letter' ww_value='2'/>" ); break;
+			case ( '3' == letter ) : process.stdout.write( "<td class='bonus letter' ww_value='3'/>" ); break;
+			case ( '4' == letter ) : process.stdout.write( "<td class='bonus letter' ww_value='4'/>" ); break;
+			case ( '@' == letter ) : process.stdout.write( "<td class='bonus word' ww_value='2'/>" ); break;
+			case ( '#' == letter ) : process.stdout.write( "<td class='bonus word' ww_value='3'/>" ); break;
+			case ( '$' == letter ) : process.stdout.write( "<td class='bonus word' ww_value='4'/>" ); break;
+
+			case ( letter >= 'A' && 
+				   letter <= 'Z' ) : process.stdout.write( "<td class='static' ww_value='" + letter + "'/>" ); break;
+
+		}
+	});
+
+	process.stdout.write( ' </tr>\n' );
+})
+
+rl.on('close', function(line){
+	console.log( '</tbody>' );
+})
