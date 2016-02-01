8fe3c4ac2396467f32d0037c3d51e4debaff20e8
Jason Haines <github@jhaines.name>
2016-02-01 11:18:50 +1100

    Audio cached at load time - should fix play delay

--- client/js/audio.js
+++ client/js/audio.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function AudioProxy() {
+	this.audio = {
+		'endTurn' : new Audio( '/audio/endturn.mp3' ),
+	};
+
+	this._play = function( audioKey ) {
+		this.audio[ audioKey ].play();
+	}
+
 	this.endTurn = function() {
-		var audio = new Audio('/audio/endturn.mp3');
-		audio.play();
+		this.audio['endTurn'].play();
 	}
 }
