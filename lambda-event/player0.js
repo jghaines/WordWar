8119cc1f6cb96bb153f2ac4c115fe2d4e40c22fd
Jason Haines <github@jhaines.name>
2016-02-28 16:01:57 +1100

    VS Code launch files and sample lambda-event s

---
+++ lambda-event/player0.js
@@ -0,0 +1,7 @@
+// Sample event data 
+module.exports = {
+        "playerIndex"	:	"0",
+        "gameId"		:	"aa00",
+        "turnIndex"		:	"4",
+        "word"			:	"YING"
+}
---
+++ lambda-event/player1.js
@@ -0,0 +1,7 @@
+// Sample event data 
+module.exports = {
+        "playerIndex"	:	"1",
+        "gameId"		:	"aa00",
+        "turnIndex"		:	"4",
+        "word"			:	"YANG"
+}
