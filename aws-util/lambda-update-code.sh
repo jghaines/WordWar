3618d9840be0e11154bfaf85f69b2db7c53f07a6
Jason Haines <github@jhaines.name>
2016-03-10 15:50:16 +1100

    update .vscode/launch.json and aws-util for new ./lambda/ subdirectory

--- aws-util/lambda-update-code.sh
+++ aws-util/lambda-update-code.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+echo '--- Zip'
 mkdir tmp
 rm -f ./tmp/lambda.zip
-zip ./tmp/lambda.zip ./package.json ./*.js
+cd lambda/
+zip ../tmp/lambda.zip -r *
+cd ..
+echo '--- S3 copy'
 aws s3 cp ./tmp/lambda.zip s3://wordwar-config/lambda/lambda.zip
 
+echo '--- Lambda update'
 aws lambda update-function-code --function-name WordWar-GetGameLambda-J5MWSRAOO5Y4 --s3-bucket wordwar-config --s3-key lambda/lambda.zip
 aws lambda update-function-code --function-name WordWar-PutPlayLambda-2CV93YOFZ4AB --s3-bucket wordwar-config --s3-key lambda/lambda.zip
