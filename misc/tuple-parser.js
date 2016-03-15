b7e81fe8ded829ace16febe8f372fd87df9a67f4
Jason Haines <github@jhaines.name>
2016-03-15 20:04:13 +1100

    Tuple parser that shows counts of every (two-letter) tuple in sowpods

---
+++ misc/tuple-parser.js
@@ -0,0 +1,48 @@
+// search through sowpods and count every (two-letter) tuple
+// print them from most to least common
+
+'use strict';
+
+
+var fs = require('fs');
+
+var readable = fs.createReadStream( __dirname + '/sowpods.txt', {
+    encoding : 'utf8', // otherwise we get a binary Buffer
+    highWaterMark : 1}); // 1-byte chunks
+
+var previous = ' ';
+var tuple = {};
+var array = [];
+
+readable.on( 'data', ( data ) => {
+    var current = data[0].toLowerCase();
+
+    if ( previous && previous.match( /[a-z]/ ) &&
+         current  && current.match( /[a-z]/ )) {
+        tuple[previous] = tuple[previous] || {};
+        tuple[previous][current] = tuple[previous][current] || 0; 
+
+        tuple[previous][current]++;
+    }
+    previous = current;
+});
+
+readable.on( 'end', ( data ) => {
+    Object.keys(tuple).sort().forEach( function( first ) {
+        Object.keys(tuple[first]).sort().forEach( function( second ) {
+            array.push( { tuple : first+second, count : tuple[first][second] } );
+        });
+    });
+    
+    array.sort( function(a,b) {
+       if ( a.count < b.count ) return 1; 
+       if ( a.count > b.count ) return -1;
+       return 0; 
+    });
+    
+    array.forEach( function(el) {
+       console.log( JSON.stringify(el) ); 
+    });
+    
+    process.exit();
+});
