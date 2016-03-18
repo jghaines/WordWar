61956e5f7df72341b81010d21a0483f18acebf9f
Jason Haines <github@jhaines.name>
2016-03-18 20:08:15 +1100

    Show AttackMultiplier in score section

--- client/js/score.js
+++ client/js/score.js
@@ -1,22 +1,28 @@
 'use strict';
 
 function ScoreView() {
-    this._getUiForPlayerIndex = function( playerIndex ) {
-        return $( 'span.score.player-' + playerIndex );
-    }
-
 	this.flashScore = function( playerIndex ) {
-		flash( this._getUiForPlayerIndex( playerIndex ), 'flash-large-pulse' );
+		flash( this._scoreUi[playerIndex], 'flash-large-pulse' );
 	}
 
     this.setPlay = function( play ) {
         this._plays[play.playerIndex] = play;
         
-        var ui = this._getUiForPlayerIndex( play.playerIndex );
-
-        ui.text( play.startTurnScore );
-        ui.toggleClass( 'lost', play.lost );
+        this._scoreUi[play.playerIndex].text( play.startTurnScore );
+        this._scoreUi[play.playerIndex].toggleClass( 'lost', play.lost );
+        
+        this._attackMultiplierUi[play.playerIndex].text( 'x' + play.attackMultiplier );
     }
 
     this._plays = [];
+    this._scoreUi = [
+        $( 'span.score.player-0' ),
+        $( 'span.score.player-1' ),
+    ];
+
+    this._attackMultiplierUi = [
+        $( 'span.attackMultiplier.player-0' ),
+        $( 'span.attackMultiplier.player-1' ),
+    ];
+            
 }
