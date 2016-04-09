15fd09911b282bf75d2d6e3139868c94da70e91d
Jason Haines <github@jhaines.name>
2016-04-09 16:03:04 +1000

    Fix #28 - off-by-1 error in lettersDistributon

--- lambda/restApi/lib/wordy-letters.js
+++ lambda/restApi/lib/wordy-letters.js
@@ -1,32 +1,32 @@
 'use strict';
 
 var _letterDistribution = 
-        Array(9).join("A") +
-        Array(2).join("B") +
-        Array(2).join("C") +
-        Array(4).join("D") +
-        Array(12).join("E") +
-        Array(2).join("F") +
-        Array(3).join("G") +
-        Array(2).join("H") +
-        Array(9).join("I") +
-        Array(1).join("J") +
-        Array(1).join("K") +
-        Array(4).join("L") +
-        Array(2).join("M") +
-        Array(6).join("N") +
-        Array(8).join("O") +
-        Array(2).join("P") +
-        Array(1).join("Q") +
-        Array(6).join("R") +
-        Array(2).join("S") +
-        Array(6).join("T") +
-        Array(4).join("U") +
-        Array(2).join("V") +
-        Array(2).join("W") +
-        Array(1).join("X") +
-        Array(2).join("Y") +
-        Array(1).join("Z");
+        Array(1+9).join("A") +
+        Array(1+2).join("B") +
+        Array(1+2).join("C") +
+        Array(1+4).join("D") +
+        Array(1+12).join("E") +
+        Array(1+2).join("F") +
+        Array(1+3).join("G") +
+        Array(1+2).join("H") +
+        Array(1+9).join("I") +
+        Array(1+1).join("J") +
+        Array(1+1).join("K") +
+        Array(1+4).join("L") +
+        Array(1+2).join("M") +
+        Array(1+6).join("N") +
+        Array(1+8).join("O") +
+        Array(1+2).join("P") +
+        Array(1+1).join("Q") +
+        Array(1+6).join("R") +
+        Array(1+2).join("S") +
+        Array(1+6).join("T") +
+        Array(1+4).join("U") +
+        Array(1+2).join("V") +
+        Array(1+2).join("W") +
+        Array(1+1).join("X") +
+        Array(1+2).join("Y") +
+        Array(1+1).join("Z");
 
 var _getLetter = function() {
     return _letterDistribution.charAt(Math.floor(Math.random() * _letterDistribution.length));
