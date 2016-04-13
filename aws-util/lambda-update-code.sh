1697822190b5aede6296a3d32813a3a367b20edd
Jason Haines <github@jhaines.name>
2016-04-13 19:09:28 +1000

    Use --publish to keep Lambda versions

--- aws-util/lambda-update-code.sh
+++ aws-util/lambda-update-code.sh
@@ -3,6 +3,6 @@
 $(dirname $0)/s3-upload-lambda.sh
 
 echo '--- Lambda update'
-aws lambda update-function-code --function-name WordWar-GetGameLambda-J5MWSRAOO5Y4 --s3-bucket wordwar-config --s3-key lambda/lambda.zip  --profile wordwar
-aws lambda update-function-code --function-name WordWar-PutPlayLambda-2CV93YOFZ4AB --s3-bucket wordwar-config --s3-key lambda/lambda.zip  --profile wordwar
-aws lambda update-function-code --function-name echo --s3-bucket wordwar-config --s3-key lambda/lambda.zip  --profile wordwar
+aws lambda update-function-code --function-name WordWar-GetGameLambda-J5MWSRAOO5Y4 --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
+aws lambda update-function-code --function-name WordWar-PutPlayLambda-2CV93YOFZ4AB --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
+aws lambda update-function-code --function-name WordWar-echo                       --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
