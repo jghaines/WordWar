502a0fa75027ef6b28378c6e63aa8acf4796ee91
Jason Haines <github@jhaines.name>
2016-04-21 18:01:21 +1000

    cfn-*-stack - fixed local paths. Deploy to 'dev' environment

--- aws-util/cfn-create-stack.sh
+++ aws-util/cfn-create-stack.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 clear
-aws s3 cp cloudformation.json s3://wordwar-config/cloudformation/cloudformation.json && \
+aws s3 cp ./cloudformation/cloudformation.json s3://wordwar-config/cloudformation/cloudformation.json && \
 \
-aws cloudformation create-stack \
-  --region us-west-2 \
-  --template-url https://s3-us-west-2.amazonaws.com/wordwar-config/cloudformation/cloudformation.json \
-  --stack-name WordWar \
-  --profile wordwar
+aws cloudformation  create-stack \
+  --region          us-west-2 \
+  --template-url    https://s3-us-west-2.amazonaws.com/wordwar-config/cloudformation/cloudformation.json \
+  --stack-name      WordWar-dev \
+  --profile         wordwar \
+  --parameters      ParameterKey=Env,ParameterValue=dev_
+  
