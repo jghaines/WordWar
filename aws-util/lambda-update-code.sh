b8ad7e0ff22951bac21772dd71978f60b4ac903b
Jason Haines <github@jhaines.name>
2016-04-23 18:46:33 +1000

    Deploy to GetGame,PutGame etc

--- aws-util/lambda-update-code.sh
+++ aws-util/lambda-update-code.sh
@@ -3,13 +3,13 @@
 $(dirname $0)/s3-upload-lambda.sh
 
 echo '--- Lambda update'
-aws lambda update-function-code --function-name WordWar-GetGameLambda-J5MWSRAOO5Y4 --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
-aws lambda update-function-code --function-name WordWar-PutPlayLambda-2CV93YOFZ4AB --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
-aws lambda update-function-code --function-name WordWar-echo                       --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
+aws lambda update-function-code --function-name GetGame --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
+aws lambda update-function-code --function-name PutPlay --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
+aws lambda update-function-code --function-name Echo    --s3-bucket wordwar-config --s3-key lambda/lambda.zip --publish --profile wordwar
 
 description=`git rev-parse --short HEAD`
-aws lambda update-function-configuration --function-name WordWar-GetGameLambda-J5MWSRAOO5Y4 --description $description --profile wordwar
-aws lambda update-function-configuration --function-name WordWar-PutPlayLambda-2CV93YOFZ4AB --description $description --profile wordwar
-aws lambda update-function-configuration --function-name WordWar-echo                       --description $description --profile wordwar
+aws lambda update-function-configuration --function-name GetGame --description $description --profile wordwar
+aws lambda update-function-configuration --function-name PutPlay --description $description --profile wordwar
+aws lambda update-function-configuration --function-name Echo    --description $description --profile wordwar
 
 say 'Lambda deployed'
